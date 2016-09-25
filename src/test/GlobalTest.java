79eb807dc767be40fec37ea54ef3168f673c8bff
LostArchives <valentin.mullet@gmail.com>
2016-09-25 04:08:15 +0200

    Change output System - More results in output

--- src/test/GlobalTest.java
+++ src/test/GlobalTest.java
@@ -6,8 +6,8 @@ import model.Knapsack;
 public class GlobalTest {
 
 	static int pasEvaluation = 100;
-	static int maxEvaluation = 1000;
-	static int nbExecution = 10;
+	static int maxEvaluation = 5000;
+	static int nbExecution = 500;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -16,15 +16,15 @@ public class GlobalTest {
 		CalculationMethod cm = new CalculationMethod(new Knapsack("resources/ks_1000.dat"));
 		CalculationLauncher cl = new CalculationLauncher(cm,nbExecution,pasEvaluation,maxEvaluation);
 		
-		//cl.LaunchCalculation(cm,"recherche","rechercheAleatoire");
-		//cl.LaunchCalculation(cm,"marche","marcheAleatoire");
-		cl.LaunchCalculation(cm,"hillclimber","output/hillClimber");
-		cl.LaunchCalculation(cm,"hillclimberfirst","output/hillClimberFirst");
+		//cl.LaunchCalculation(cm,"recherche","output/rechercheAleatoire");
+		//cl.LaunchCalculation(cm,"marche","output/marcheAleatoire");
+		//cl.LaunchCalculation(cm,"hillclimber","output/hillClimber");
+		//cl.LaunchCalculation(cm,"hillclimberfirst","output/hillClimberFirst");
 		
 		
 		///Test avec évaluation fixe à 1000
-		//cl.LaunchCalculationWithFixedEval(cm,"recherche","rechercheAleatoire",1000);
-		//cl.LaunchCalculationWithFixedEval(cm,"marche","marcheAleatoire",1000);
+		cl.LaunchCalculationWithFixedEval(cm,"recherche","output/rechercheAleatoire",1000);
+		cl.LaunchCalculationWithFixedEval(cm,"marche","output/marcheAleatoire",1000);
 		//cl.LaunchCalculationWithFixedEval(cm,"hillclimber","hillClimber",500);
 		//cl.LaunchCalculationWithFixedEval(cm,"hillclimberfirst","hillClimberFirst",500);
 	}
